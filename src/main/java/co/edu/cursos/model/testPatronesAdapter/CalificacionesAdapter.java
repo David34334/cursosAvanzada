6d10a296823c6082ff2ec8adc34dc1c0c5d130c6
Brayan Urbina <burbinagomez@gmail.com>
2020-10-20 10:38:14 -0500

    adapter

---
+++ src/main/java/co/edu/cursos/model/testPatronesAdapter/CalificacionesAdapter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.cursos.model.testPatronesAdapter;
+
+import java.util.List;
+
+/**
+ *
+ * @author b_urb
+ */
+public class CalificacionesAdapter extends otros implements caracteristicasCalificaciones{
+
+    @Override
+    public String getPreguntas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getRespuestas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getCalificacion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
